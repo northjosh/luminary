12ae95fdd750dbfed508447f870b153fabdb1e05
Joshua Mensah <joshmensah18@gmail.com>
2025-08-22 18:24:16 +0000

    feat: add local pg support

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,11 +1,24 @@
-import { neon, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import ws from "ws";
+import { neon, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle as pdrizzle } from 'drizzle-orm/postgres-js';
+import ws from 'ws';
 
 neonConfig.webSocketConstructor = ws;
 
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
 // neonConfig.poolQueryViaFetch = true
 
-const sql = neon(process.env.DATABASE_URL || "");
-export const db = drizzle(sql);
+const createDb = () => {
+  const databaseUrl = process.env.DATABASE_URL;
+  if (!databaseUrl) {
+    throw new Error('DATABASE_URL is required');
+  }
+
+  if (databaseUrl.includes('neon.tech')) {
+    const sql = neon(databaseUrl);
+    return drizzle(sql);
+  }
+    return pdrizzle(databaseUrl);
+};
+
+export const db = createDb();
